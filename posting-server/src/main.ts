10685bbb259196022b89073a0ca1d900dc824a7a
sckimynwa <tsb03672@naver.com>
2019-11-07 11:39:49 +0900

    dependency added

--- posting-server/src/main.ts
+++ posting-server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      allowedHeaders: ['Range', 'Authorization', 'Content-Type'],
+      exposedHeaders: [
+        'Accept-Ranges',
+        'Content-Range',
+        'Content-Encoding',
+        'Content-Length',
+      ],
+    },
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
